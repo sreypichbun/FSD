1df6b94bebf1152dedba3537342ee8fb6e4ef6f5
Sreypich <sreypichbun2122@gmail.com>
2026-04-25 11:33:31 +1000

    add university menu and student system

---
+++ The Univeristy System.py
@@ -0,0 +1,31 @@
+def University_System_Menu():
+    while True:
+        choice=input("University System: (A)dmin, (S)tudent, or X: ").upper()
+        if choice =="A":
+            #Admin_System() TO BE BUILD
+            pass
+        elif choice =="S":
+            Student_System()
+            pass
+        elif choice =="X":
+            print("Thank You")
+            break
+        else:
+            print("Error")    
+        
+
+            
+    
+def Student_System():
+    while True:
+        Choice=input("Student System (l/r/x): ").lower()
+        if Choice=="r":
+            #Student_Sign_Up
+            pass
+        elif Choice=="l":
+            #Student_Sign_In
+            pass
+        elif Choice =="x":
+            break
+            
+University_System_Menu()
