5f66b14698b1a6c96de6f509a235c3d6726ece2b
HonHoi-Chan <HonHoi.Chan@student.uts.edu.au>
2026-04-28 23:17:41 +1000

    Add files via upload

---
+++ src/LoginController.py
@@ -0,0 +1,22 @@
+from student_controller import StudentControllerClass
+class LoginController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        
+        self.view.login_button.config(command=self.on_login_click) # actions when the login button is clicked
+        self.view.cancel_button.config(command=self.view.login_window.destroy) # actions when the cancel button is clicked
+
+    def on_login_click(self):
+        # Get email address and password from user input in view
+        email = self.view.email_text.get()
+        password = self.view.password_text.get()
+        
+        # Controller asks the Model (Database class) to check credentials
+        # dependent on the DB set up, come back after completing DB logic
+        if self.model.validate_email(email) and self.model.validate_password(password):
+            self.view.update_message("Login Successful!", color="green") # Update the message in the view to show success
+            self.view.login_window.destroy() # Close the login window
+            self.open_enrolment_system() # Call a function to open the enrolment system window
+        else:
+            self.view.update_message("Please verify your Email/Password, and whether the “Caps Lock” key is pressed on your keyboard, and retry.", color="red") # Update the message in the view to show failure
